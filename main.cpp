5ea0461a6d4fa2b73cf1b84620e7904f91a9d438
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2019-05-22 18:20:55 -0400

    Se realizaron los métodos para validar el ingreso de una palabra. El algoritmo consiste en recibir un String y convertirlo en un Array

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <cstdlib>
+#include "State.hpp"
+#include "Function.hpp"
 
 using namespace std;
 
+string inputWord();
+void stringToArray(char *A, string word);
+
 int main(){
 
-    
+    //Ingreso de estado inicial (PENDIENTE)
+     state *p = nullptr;
 
+    //Ingreso de la palabra
+    string wordString = inputWord();
+    char *Word = new char[wordString.length()-1];
+    stringToArray(Word, wordString);
+
+}
+
+string inputWord(){
+    string word;
+    cout << "Ingrese la palabra" << endl;
+    cin >> word;
+    return word;
+}
+
+void stringToArray(char *A, string word){
+    for (int i = 0; i < word.length(); i++){
+        A[i] = word[i];
+    }
 }

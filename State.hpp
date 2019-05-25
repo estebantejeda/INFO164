7ca478cfaf4751c87f9be7c99636c62f6c2d2d18
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2019-05-24 23:23:43 -0400

    Se realizaron cambios en el State. Se agregó un nombre para el estado junto al seguimiento de estos. Se agregó un puntero hacia las transiciones, un constructor para que la comprobación del estado inicie en false y se creó un método que genera el primer estado

--- State.hpp
+++ State.hpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <stdlib.h>
+#include "Function.hpp"
 
 using namespace std;
 
 struct State{
     string name; //Nombre del Estado
+    int ei; //Estado Inicio
+    int ef; //Estado Llegada
     bool fin; //Comprobación de Estado Final
+    function *fun; //Puntero hacia la Transición
     State *next; //Puntero al siguiente Estado
+
+    State(){ //Constructor
+        fin = false; //Estado final se inicializa como falso
+    }
+
 }; typedef struct State state;
+
+void createInitialState(state **S, string name){
+    state *p = new state;
+    p->name = name;
+    p->next = *S;
+    *S = p;
+}

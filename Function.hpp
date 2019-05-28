f0422bff54188464aea0dc9364cb9ef6fbab0ae2
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2019-05-27 21:39:26 -0400

    Se creó una función en Function.hpp para almacenar las variables de la estructura

--- Function.hpp
+++ Function.hpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 struct Function{
+    string name; //Nombre de Estado de Destino
     char letter; //Letra de la Función de Transición
     Function *next; //Puntero a la siguiente Función
 }; typedef struct Function function;
+
+void createFunction(function **F, string name, char letter){
+    //Variables Iniciales
+    function *p = new function;
+    p->name = name;
+    p->letter = letter;
+    p->next = nullptr;
+
+    //Creación de Función
+    p->next = *F;
+    *F = p;
+}
